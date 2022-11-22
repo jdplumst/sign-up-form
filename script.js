305b3d90981a833268970b4d52fe8b39b11235ed
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-11-21 19:44:58 -0500

    Disable submit button when password fields do not match

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const password = document.querySelector('#password');
 const confirm = document.querySelector('#confirm-password');
 const error = document.querySelector('.error-msg');
+const submit = document.querySelector('button');
 
+// Checks if the passwords in the Password and Confirm Password
+// fields match
 function confirmPasswordMatch() {
     if (password.value === confirm.value) {
         error.textContent = '';
         password.classList.remove('error');
         confirm.classList.remove('error');
+        submit.disabled = false;
     } else {
         error.textContent = '* Passwords do not match';
         password.classList.add('error');
         confirm.classList.add('error');
+        submit.disabled = true;
     }
 };
 
